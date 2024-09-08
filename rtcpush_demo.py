007e952347e68773836e854b4162969e54a9aeed
90houlaoheshang <907333918@qq.com>
2024-09-08 21:28:29 +0800

    静音音频

--- rtcpush_demo.py
+++ rtcpush_demo.py
@@ -6,18 +6,20 @@ from typing import Union
 from PIL import Image
 import numpy as np
 import aiohttp
-from aiortc import RTCPeerConnection, RTCSessionDescription, MediaStreamTrack, VideoStreamTrack, AudioStreamTrack
+from aiortc import RTCPeerConnection, RTCSessionDescription, MediaStreamTrack, VideoStreamTrack, AudioStreamTrack, \
+    RTCRtpSender
 from av import Packet
 from av.frame import Frame
-from av import VideoFrame
+from av import AudioFrame, VideoFrame
 import fractions
+from fractions import Fraction
 import signal
 
 pc = None
-ast = None
+ast: None | AudioStreamTrack = None
 pst = None
-audio_tracker = None
-video_tracker = None
+audio_sender: None | RTCRtpSender = None
+video_sender = None
 
 
 async def post(url, data):
@@ -30,11 +32,10 @@ async def post(url, data):
 
 
 class PlayerStreamTrack(MediaStreamTrack):
-    kind = "video"
-
-    def __init__(self):
+    def __init__(self, kind="video"):
         super().__init__()
         self.frame_count = 0
+        self.kind = kind
 
         img = Image.open('girl.jpeg')
         try:
@@ -43,21 +44,24 @@ class PlayerStreamTrack(MediaStreamTrack):
             img.close()
 
         new_frame = VideoFrame.from_ndarray(img_array, format="rgb24")
-        new_frame.time_base = fractions.Fraction(1, 25)
+        new_frame.time_base = fractions.Fraction(1, 25)  # 视频25fps
         self.new_frame = new_frame
 
     async def recv(self) -> Union[Frame, Packet]:
-        self.frame_count += 1
-        self.new_frame.pts = self.frame_count
-        return self.new_frame
+        if self.kind == "video":
+            self.frame_count += 1
+            self.new_frame.pts = self.frame_count
+            return self.new_frame
+        else:
+            return get_audio_frame()
 
 
 async def get_pc(push_url):
     global pc
     global ast
     global pst
-    global audio_tracker
-    global video_tracker
+    global audio_sender
+    global video_sender
 
     pc = RTCPeerConnection()
 
@@ -67,11 +71,11 @@ async def get_pc(push_url):
         if pc.connectionState == "failed":
             await pc.close()
 
-    ast = AudioStreamTrack()
-    audio_tracker = pc.addTrack(ast)
+    ast = PlayerStreamTrack(kind="audio")
+    audio_sender = pc.addTrack(ast)
 
     pst = PlayerStreamTrack()
-    video_tracker = pc.addTrack(pst)
+    video_sender = pc.addTrack(pst)
 
     await pc.setLocalDescription(await pc.createOffer())
     answer = await post(push_url, pc.localDescription.sdp)
@@ -92,14 +96,33 @@ async def gracefully_close():
     except:
         pass
     try:
-        not audio_tracker or await audio_tracker.stop()
+        not audio_sender or await audio_sender.stop()
     except:
         pass
     try:
-        not video_tracker or await video_tracker.stop()
+        not video_sender or await video_sender.stop()
     except:
         pass
     loop.stop()
+
+
+pts = 0
+time_base = Fraction(1, 16000)
+
+
+def get_audio_frame():
+    frame = np.zeros(320, dtype=np.float32)
+    frame = (frame * 32767).astype(np.int16)
+    new_frame = AudioFrame(format='s16', layout='mono', samples=frame.shape[0])
+    new_frame.planes[0].update(frame.tobytes())
+    new_frame.sample_rate = 16000
+
+    global pts
+    new_frame.pts = pts
+    pts += 320  # 16000/50，音频50fps
+    new_frame.time_base = time_base
+
+    return new_frame
 
 
 def safely_exit():
